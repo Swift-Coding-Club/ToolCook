6e99bf7360634b04a1fda4a7047df94dde3e23df
강동영 <rkdehddud96@siksinhot.com>
2023-03-02 10:20:23 +0900

    MusicPlaylist starter

---
+++ MusicPlaylist/Rx/MusicPlaylist/MusicPlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlaylistApp.swift
+//  MusicPlaylist
+//
+//  Created by dongyeongkang on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
